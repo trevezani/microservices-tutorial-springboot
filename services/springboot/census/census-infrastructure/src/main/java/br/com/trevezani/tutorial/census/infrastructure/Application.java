7b5acb9c129cd1bdb7d2b696b375ce6fe160edd0
Alberto Trevezani <trevezani@gmail.com>
2020-06-02 17:05:07 -0300

    Disabled autoconfiguration

--- services/springboot/census/census-infrastructure/src/main/java/br/com/trevezani/tutorial/census/infrastructure/Application.java
+++ services/springboot/census/census-infrastructure/src/main/java/br/com/trevezani/tutorial/census/infrastructure/Application.java
@@ -1,10 +1,13 @@
 package br.com.trevezani.tutorial.census.infrastructure;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 @ComponentScan("br.com.trevezani.tutorial.census")
 public class Application {
 
